265e1657cbce25b2e4704b23fef14195ef9e5be8
yemeal <95481119+yemeal@users.noreply.github.com>
2026-04-08 20:51:24 +0700

    chore: modify update_modules.bat to update submodules remotely
    Updated the script to fetch the latest remote commits for Git submodules.

--- update_modules.bat
+++ update_modules.bat
@@ -1,7 +1,7 @@
 @echo off
-echo Updating Git modules...
-git submodule update --init --recursive
+echo Updating Git modules to latest remote commits...
+git submodule update --init --recursive --remote
 echo.
-echo All submodules have been successfully downloaded to the frontend/ and backend/ folders!
+echo All submodules have been successfully updated to the latest branches in frontend/ and backend/ folders!
 echo Now you can run docker-compose.
 pause
